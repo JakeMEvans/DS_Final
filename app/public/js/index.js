773f276fb2d5de80717947b6aeb1d9e051ce9c02
kaykenny <kaykenny@iu.edu>
2020-10-26 19:31:15 -0400

    working on members html

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,25 +1,8 @@
 var app = new Vue({
   el: '#ocfrPage',
   data: {
-      certList: [],
-      cmList: [],
       memberList: [],
-      newCertificationForm: {},
-      newMemberForm:{}
-  },
-
-  methods: {
-    newCertificationData() {
-      return {
-        CertificationID: "",
-        certAgency: "",
-        certificationName: "",
-        expirationDate: "",
-      }
-    },
-
-    newMemberData() {
-      return {
+      newMemberForm:{
         personID: "",
         firstName:"",
         lastName: "",
@@ -30,10 +13,12 @@ var app = new Vue({
         startDate: "",
         radioNum: "",
         stationNum: "",
-        isActive: "",
-      }
-    },
-
+        isActive: ""
+  }
+ },
+methods:{
+  
+}
   // handleNewCertificationForm: ( evt ) {
   //     console.log("Certication form submitted");
   //
@@ -53,75 +38,23 @@ var app = new Vue({
   //     });
   //   },
 
-    handleNewMemberForm ( evt ) {
-      console.log("Member form submitted!");
-      fetch('api/members/memberpost.php', {
-        method: 'POST',
-        body: JSON.stringify(this.newMemberForm),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "Accept": "application/json"
-        }
-      })
-      .then( response => response.json() )
-      .then( json => {
-        console.log("Returned from post:", json);
-        this.memberList = json;
-        this.newMemberForm = this.newMemberData();
-      });
-    }
-  },
-
-  created() {
-    fetch("api/certifications/")
-    .then( response => response.json() )
-    .then( json => {
-      this.certList = json;
-
-      console.log(json)}
-    );
-
-    this.newCertificationForm = this.newCertificationData();
-  }
-})
-    fetch("api/certifications/certificationindex.php")
-    .then( respose => response.json() )
-    .then( json => {
-        this.cmList = json;
-
-        console.log(json)}
-      );
-
-      fetch("api/members/memberindex.php", {
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "Accept": "application/json"
-        }
-      })
-      .then( respose => response.json() )
-      .then( json => {
-        this.memberList = json;
-
-        console.log(json)}
-      );
-
-      fetch("api/members/memberpost.php")
-        .then( response => response.json() )
-        .then( json => {
-          this.memberList = json;
-
-          console.log(json)}
-        );
-
-        fetch("api/members/memberpost.php")
-        .then( respose => response.json() )
-        .then( json => {
-          this.memberList = json;
-
-          console.log(json)}
-        );
-
-    this.newCertificationForm = this.newCertificationData();
-    this.newMemberForm = this.newMemberData();
-      }
-        })
+// methods: {
+//     handleNewMemberForm ( evt ) {
+//       console.log("Member form submitted!");
+//       fetch('api/members/memberpost.php', {
+//         method: 'POST',
+//         body: JSON.stringify(this.newMemberForm),
+//         headers: {
+//           "Content-Type": "application/json; charset=utf-8",
+//           "Accept": "application/json"
+//         }
+//       })
+//       .then( response => response.json() )
+//       .then( json => {
+//         console.log("Returned from post:", json);
+//         this.memberList = json;
+//         this.newMemberForm = this.newMemberData();
+//       });
+//     }
+//   },
+// })
